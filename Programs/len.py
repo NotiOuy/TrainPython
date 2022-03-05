ec48ff03e358c5c5d1dafefa362955bd450aa1f2
NotiOuy <immaxnor@gmail.com>
2022-03-05 21:02:34 +0200

    Add file len.py

---
+++ Programs/len.py
@@ -0,0 +1,15 @@
+
+example = "Hello, Python!"
+
+#len function count number of the list for example Hello Python! has 13 chracters.
+print(len(example))
+#All Big characters
+print(example.upper())
+#All Little characters
+print(example.lower())
+#To find character
+print(example.find("y"))
+#Replace characters
+print(example.replace("Hello", "Bye"))
+print("!" in example)
+print(example.title())
