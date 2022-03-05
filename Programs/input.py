96adb85420fea6040f2ee23c56f367777193398e
NotiOuy <immaxnor@gmail.com>
2022-03-05 15:23:36 +0200

    Add to file input.py some changens

--- Programs/input.py
+++ Programs/input.py
@@ -2,5 +2,6 @@
 number_prefer = input("What number do you prefer? ")
 
 print("Your prefer number is " + number_prefer)
+print(type(number_prefer))
 
 print(int(input("what is your age? and minus at your age 3 years: ")) - 3)
