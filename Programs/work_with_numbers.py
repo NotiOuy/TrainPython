24060d1849475f128be1d810720d5dc5006d6a04
NotiOuy <immaxnor@gmail.com>
2022-03-08 18:18:38 +0200

    Add file work_with_numbers.py

---
+++ Programs/work_with_numbers.py
@@ -0,0 +1,10 @@
+import math
+
+
+number = 4.6987
+negetive_number = -8.5965
+
+print("Round number: "+ str(round(number)))
+print("Make number always positive even it is negative: " + str(abs(negetive_number)))
+print("The ceil of the numbers: " + str(math.ceil(number)) + " " + str(math.ceil(negetive_number)))
+print("The floor of the numbers: " + str(math.floor(number)) + " " + str(math.floor(negetive_number)))
