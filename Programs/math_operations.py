be90b59bc42e72278f0c000d35e32030ef2028d7
NotiOuy <immaxnor@gmail.com>
2022-03-08 16:33:12 +0200

    Add file math_operations.py

---
+++ Programs/math_operations.py
@@ -0,0 +1,26 @@
+
+print("The sum of the numbers 20 and 6: " + str(20 + 6))
+
+number1 = 68
+number2 = 3
+
+sum_numbers = number1 + number2
+print("The sum of the two numbers: " + str(sum_numbers))
+
+division_numbers = number1 / number2
+print("Division of the two numbers: " + str(division_numbers))
+
+multiplication_numbers = number1 * number2
+print("Multiplication of the two numbers: " + str(multiplication_numbers))
+
+difference_numbers = number1 - number2
+print("The difference of the two numbers: " + str(difference_numbers))
+
+operator_remainder = number1 % number2
+print("Use remainder operator (%): " + str(operator_remainder))
+
+squer_numbers = number1 // number2
+print("The number 1 in root the number 2: " + str(squer_numbers))
+
+operator_power = number1 ** number2
+print("The number 1 in the power of the number 2: " + str(operator_power))
